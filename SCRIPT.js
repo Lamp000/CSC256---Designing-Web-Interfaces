992f1f6f3ee1d824dfcaee02d8d81b413bdf2e51
Lamp000 <jack06schneider@gmail.com>
2026-05-30 14:12:36 -0700

    Add files via upload

---
+++ SCRIPT.js
@@ -0,0 +1,5 @@
+function loadInfo(){
+    document.getElementById("hMajor").textContent = "Major: Computer Science";
+    document.getElementById("hEmail").textContent = "Email: JSCHNEIDER93339@uat.edu";
+    document.getElementById("hGradDate").textContent = "Expected Graduation Date: 6/12/28";
+}
