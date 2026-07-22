acee37d5291d306819bc949c4c722c7b713886e6
Lamp000 <jack06schneider@gmail.com>
2026-07-22 11:34:43 -0700

    Add files via upload

--- 81chessboard/chess.js
+++ 81chessboard/chess.js
@@ -12,36 +12,8 @@ function createBoard(row, col){
         chess.appendChild(tr);
     }
 }
-let arrSetupChess = [
-    ["Rook", "Knight", "Bishop", "Queen", "King", "Bishop", "Knight", "Rook"],
-    ["Pawn", "Pawn", "Pawn", "Pawn", "Pawn", "Pawn", "Pawn", "Pawn"],
-    ["Pawn", "Pawn", "Pawn", "Pawn", "Pawn", "Pawn", "Pawn", "Pawn"],
-    ["Rook", "Knight", "Bishop", "Queen", "King", "Bishop", "Knight", "Rook"],
-];
-function chessPieces(){
-    for(let i=0;i<8;i++){
-        for(let o=0;o<8;o++){
-            if(i<2 || i>5){
-                let element = document.createElement("img");
-                element.setAttribute("src", arrSetupChess[["0","1","","","","","2","3"][i]][o]+["W","W","B","W","B","B","B","B"][i]+".png");
-                element.setAttribute("class", "piece")
-                element.setAttribute("id", "Piece"+i+o)
-                document.getElementById("pieceLayer").appendChild(element);
-            }
-        }
-    }
-}
+
 function setPieces(){
     document.getElementById("pieceLayer").innerHTML="";
-    chessPieces();
-    for(let i=0;i<8;i++){
-        for(let o=0;o<8;o++){
-            if(i<2 || i>5){
-                let piece = document.getElementById("Piece"+i+o);
-                let square = document.getElementById(["a","b","c","d","e","f","g","h"][o]+(i+1))
-                piece.style.top=(square.offsetTop+square.offsetHeight*2/7)+"px";
-                piece.style.left=(square.offsetLeft+square.offsetWidth*2/7)+"px";
-            }
-        }
-    }
+    
 }
